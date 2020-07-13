d6ce766275fd6a4936d3851bc0cdd0012b869ead
Jonathan <jal18@ic.ac.uk>
2020-07-14 00:47:16 +0100

    Fixed, renders quad and now uses vertex buffers.

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -1,6 +1,7 @@
-#version 400
+#version 450
 
+layout(location = 0) in vec4 in_Color;
 layout(location = 0) out vec4 out_Color;
 void main() {
-  out_Color = vec4( 0.0, 0.4, 1.0, 1.0 );
+  out_Color = in_Color; // vec4( 1.000, 0.412, 0.706, 1.0 );
 }
--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -1,5 +1,11 @@
-#version 400
+#version 450
+
+layout(location = 1) in vec4 colo;
+layout(location = 0) in vec4 posi;
+layout(location = 0) out vec4 v_Color;
+
 void main() {
-    vec2 pos[3] = vec2[3]( vec2(-0.7, 0.7), vec2(0.7, 0.7), vec2(0.0, -0.7) );
-    gl_Position = vec4( pos[gl_VertexIndex], 0.0, 1.0 );
+    // vec2 pos[3] = vec2[3]( vec2(-0.7, 0.7), vec2(0.7, 0.7), vec2(0.0, -0.7) );
+    gl_Position = posi;
+    v_Color = colo;
 }
