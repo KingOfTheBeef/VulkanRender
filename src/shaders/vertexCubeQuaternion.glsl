d9bc4afd0da4fb63c80864592f1eac7a6430b299
Jonathan L <jonathan.longman@live.com.au>
2020-09-26 21:33:10 +0100

    Derived formula for quaternion rotation instead of multiply twice

--- src/shaders/vertexCubeQuaternion.glsl
+++ src/shaders/vertexCubeQuaternion.glsl
@@ -19,9 +19,15 @@ vec4 quaternionMult(vec4 p, vec4 q) {
     return vec4(p.x * q.x - dot(p.yzw, q.yzw), p.x * q.yzw + q.x * p.yzw + cross(p.yzw, q.yzw));
 }
 
+// I derived this, just so you know ;)
+vec3 quaternionRotate(vec4 q, vec3 p) {
+    return (q.x * q.x - dot(q.yzw, q.yzw)) * p + 2 * q.yzw * dot(p, q.yzw) + 2 * q.x * cross(q.yzw, p);
+}
+
 void main() {
-    vec4 pos = quaternionMult(i_Rotation, quaternionMult(vec4(0, i_Position.xyz), vec4(i_Rotation.x, -i_Rotation.yzw)));
-    gl_Position = projection * vec4(pos.yzw, 1);
+    // vec4 pos = quaternionMult(i_Rotation, quaternionMult(vec4(0, i_Position.xyz), vec4(i_Rotation.x, -i_Rotation.yzw)));
+    vec3 pos = quaternionRotate(i_Rotation, i_Position.xyz);
+    gl_Position = projection * vec4(pos, 1);
 
     v_Color = i_Color;
 }
